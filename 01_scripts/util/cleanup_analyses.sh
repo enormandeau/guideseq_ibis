e4d124b488d6858eefb53a3de5e0d332df4bdaa7
Eric Normandeau <eric.normandeau.qc@gmail.com>
2022-12-14 11:36:15 -0500

    Report numreads per sample per step

--- 01_scripts/util/cleanup_analyses.sh
+++ 01_scripts/util/cleanup_analyses.sh
@@ -7,3 +7,4 @@ rm 06_extracted/* 2>/dev/null
 rm 07_aligned/* 2>/dev/null
 rm 08_deduplicated/* 2>/dev/null
 rm 09_sites/* 2>/dev/null
+rm 10_read_dropout/* 2>/dev/null
---
+++ 01_scripts/util/report_num_reads_per_step.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# Report number of reads per step
+
+export FOLDER="04_data"
+cd "$FOLDER"
+ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(gunzip -c "$i"*.gz | wc -l | awk '{print $1/4}')
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
+
+export FOLDER="05_trimmed"
+cd "$FOLDER"
+ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(gunzip -c "$i"*.gz | wc -l | awk '{print $1/4}')
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
+
+export FOLDER="06_extracted"
+cd "$FOLDER"
+ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(gunzip -c "$i"*.gz | grep -c "^>")
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
+
+export FOLDER="07_aligned"
+cd "$FOLDER"
+ls -1 *.sam | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(wc -l "$i"*.sam | awk '{print $1}')
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
+
+export FOLDER="08_deduplicated"
+cd "$FOLDER"
+ls -1 *.dedup | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(wc -l "$i"*.dedup | awk '{print $1}')
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
+
+export FOLDER="09_sites"
+cd "$FOLDER"
+ls -1 *.dsb | cut -d "_" -f 1 | sort -uV |
+    while read i
+    do
+        echo -e "$i\t"$(grep -v Sample "$i"*.dsb | awk '{s = s+($6+$7)}END{print s}')
+    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+cd ..
