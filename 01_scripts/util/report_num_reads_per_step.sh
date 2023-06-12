232304cb38dbf22f0c8f1a38eea5e5de02d89d4a
Eric Normandeau <eric.normandeau.qc@gmail.com>
2023-06-12 10:19:10 -0400

    Improve reads report

--- 01_scripts/util/report_num_reads_per_step.sh
+++ 01_scripts/util/report_num_reads_per_step.sh
@@ -3,56 +3,62 @@
 
 export FOLDER="04_data"
 cd "$FOLDER"
+echo -e "Sample\t""$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
     while read i
     do
         echo -e "$i\t"$(gunzip -c "$i"*.gz | wc -l | awk '{print $1/4}')
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 export FOLDER="05_trimmed"
 cd "$FOLDER"
+echo "$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
     while read i
     do
-        echo -e "$i\t"$(gunzip -c "$i"*.gz | wc -l | awk '{print $1/4}')
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+        echo $(gunzip -c "$i"*.gz | wc -l | awk '{print $1/4}')
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 export FOLDER="06_extracted"
 cd "$FOLDER"
+echo "$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.gz | cut -d "_" -f 1 | sort -uV |
     while read i
     do
-        echo -e "$i\t"$(gunzip -c "$i"*.gz | grep -c "^>")
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+        echo $(gunzip -c "$i"*.gz | grep -c "^>")
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 export FOLDER="07_aligned"
 cd "$FOLDER"
+echo "$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.sam | cut -d "_" -f 1 | sort -uV |
     while read i
     do
-        echo -e "$i\t"$(wc -l "$i"*.sam | awk '{print $1}')
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+        echo $(wc -l "$i"*.sam | awk '{print $1}')
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 export FOLDER="08_deduplicated"
 cd "$FOLDER"
+echo "$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.dedup | cut -d "_" -f 1 | sort -uV |
     while read i
     do
-        echo -e "$i\t"$(wc -l "$i"*.dedup | awk '{print $1}')
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+        echo $(wc -l "$i"*.dedup | awk '{print $1}')
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 export FOLDER="09_sites"
 cd "$FOLDER"
+echo "$FOLDER" > ../10_read_dropout/"$FOLDER"_reads.tsv
 ls -1 *.dsb | cut -d "_" -f 1 | sort -uV |
     while read i
     do
-        echo -e "$i\t"$(grep -v Sample "$i"*.dsb | awk '{s = s+($6+$7)}END{print s}')
-    done > ../10_read_dropout/"$FOLDER"_reads.tsv
+        echo $(grep -v Sample "$i"*.dsb | awk '{s = s+($6+$7)}END{print s}')
+    done >> ../10_read_dropout/"$FOLDER"_reads.tsv
 cd ..
 
 paste 10_read_dropout/*.tsv > read_dropout.tsv
